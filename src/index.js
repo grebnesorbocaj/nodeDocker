5b820835e9ac6fb1faa2f2c39fd99e5326b00c5a
grebnesorbocaj <bocajgrebnesor@gmail.com>
2020-07-27 22:40:03 -0400

    added logging and time middleware

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const myLogger = require('./utils/middleware/myLogger');
+const reqTime = require('./utils/middleware/reqTime');
 
 const app = express();
 
+app.use(myLogger);
+app.use(reqTime);
+
 app.get('/', (req, res) => {
-  res.send('hello boy');
+  res.send(`hello boy, ${req.time}`);
 });
 
 const server = app.listen(3000);
