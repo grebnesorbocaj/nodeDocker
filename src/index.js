efefa4bc8361b642d30cff6993a089f73cfefdef
grebnesorbocaj <bocajgrebnesor@gmail.com>
2020-08-16 17:04:15 -0700

    linting

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const cors = require('cors');
+const morgan = require('morgan');
+const rfs = require('rotating-file-stream');
+const path = require('path');
+
 const myLogger = require('./utils/middleware/myLogger');
 const reqTime = require('./utils/middleware/reqTime');
 
 const app = express();
 
+const logStream = rfs.createStream('access.log', {
+  interval: '1d', // rotate daily
+  path: path.join(__dirname, 'log'),
+});
+
 app.use(reqTime);
 app.use(myLogger);
+app.use(morgan('common', { stream: logStream }));
+
 app.use(cors());
 
 app.get('/', (req, res) => {
